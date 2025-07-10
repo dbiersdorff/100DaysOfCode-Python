5c4a0e7f8610636287c8f14607a2b1e574385c8a
Dawson Biersdorff <dbiersdorff8@gmail.com>
2025-07-10 10:12:02 -0600

    Day8

---
+++ Day8/cipher.py
@@ -0,0 +1,53 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def encode(word, shift_num):
+
+    ret_list = []
+
+    word_list = list(word)
+
+    for char in word_list:
+        index = alphabet.index(char.lower())
+
+        index_shifted = index + shift_num
+
+        if index_shifted > 25:
+            ret_list.append(alphabet[index_shifted - 26])
+        else:
+            ret_list.append(alphabet[index_shifted])
+    
+    print(ret_list)
+
+def decode(word, shift_num):
+
+    ret_list = []
+
+    word_list = list(word)
+
+    for char in word_list:
+
+        index = alphabet.index(char.lower())
+
+        index_shifted = index - shift_num
+
+        ret_list.append(alphabet[index_shifted])
+    
+    return ret_list
+
+running = True
+
+while running:
+
+    code_choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+    message = input("Type your message:\n")
+
+    shift_num = int(input("Type the shift number:\n"))
+
+    if code_choice == "encode":
+        print(f"Here's the encoded result: {encode(message, shift_num)}")
+    elif code_choice == "decode":
+        print(f"Here's the encoded result: {decode(message, shift_num)}")
+    else:
+        running = False
+
+
