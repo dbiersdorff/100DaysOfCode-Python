0d54bd2c4b1bc0cd79883e5be03fcdac1712cc5a
Dawson Biersdorff <dbiersdorff8@gmail.com>
2025-07-15 16:14:49 -0600

    Done.

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-#TESTING A PYTHON FILE
+from prettytable import PrettyTable, from_csv
+table = PrettyTable()
+
+table.field_names = ["name", "age"]
+table.add_row(["Dawson", 24])
+table.add_row(["Bailee", 25])
+
+print(table)
